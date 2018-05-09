1d91c811d4e315d7e56ebcf3c0b67588abfee781
Thibault Schowing <th.schow@gmail.com>
2018-05-09 15:10:49 +0200

    pictures of shit

--- Assignments/Assignment4/Scripts/layout_main.py
+++ Assignments/Assignment4/Scripts/layout_main.py
@@ -2,7 +2,7 @@ from layout import Layout
 from tools import plot_layout, plot_energies
 
 
-file_paths = ['star.txt']#, 'square.txt', 'star++.txt', 'dog.txt']
+file_paths = ['star.txt', 'square.txt', 'star++.txt', 'dog.txt']
 
 for file_path in file_paths:
     # read the file into your layout class
@@ -13,7 +13,7 @@ for file_path in file_paths:
     # plot the normal layout
 
     energies1 = layout.layout(1000)
-    plot_layout(layout, file_path + " Normal Layout")
+    plot_layout(layout, file_path + " - Normal Layout")
 
     # run the simulated annealing layout for 1000 iterations and store the total energies
     # plot the simulated annealing layout
@@ -23,5 +23,5 @@ for file_path in file_paths:
 
     # plot the total energies of the normal layout and the simulated annealing layout
 
-    plot_energies([energies1,energies2], ["Normal layout","Sim. Annealing Layout"], "Layout energies")
+    plot_energies([energies1,energies2], ["Normal layout", "Simulated Annealing Layout"], "Layout energies")
     pass
